aee12251d2b6d7f51c6884353df0481e55f650cd
Nayara <nayarakarine.araujo@gmail.com>
2024-04-02 22:39:17 -0300

    Aula de insert e select inicial

--- aula1803/TABELAS_RELACIONAIS.sql
+++ aula1803/TABELAS_RELACIONAIS.sql
@@ -1,7 +1,6 @@
 create database empregados;
 use empregados;
-create table departamento(
-	cod_depto int not null auto_increment,
-    nome varchar(30) not null,
-    constraint pk_departamento primary key(cod_depto)
-    );
+create table departamento(cod_depto int not null auto_increment,
+nome varchar(30) not null,
+constraint pk_departamento primary key(cod_depto)
+);
